7145899c8c019399c8498f1ed53b4dc2c0c12124
Tupiet <Tupiet@users.noreply.github.com>
2020-05-26 13:32:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function logIn() {
   var username = document.getElementById("username");
   var password = document.getElementById("password");
 
-  if (username.value == "John Hall" && password.value == "Hoola") {
+  if (username.value == "John Hall" && password.value == "3405") {
     window.open("../");
   }
 }

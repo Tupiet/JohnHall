350933fc1ab1e3a3becece4b8ba2846bbd0dc749
Tupiet <Tupiet@users.noreply.github.com>
2020-05-29 15:45:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function logIn() {
   var username = document.getElementById("username");
   var password = document.getElementById("password");
+  var start = document.getElementById("start");
+  var pin = document.getElementById("pinIntroduce");
 
   if (username.value == "John Hall" && password.value == "34105") {
-    window.open("../miarea");
+
   }
 }
 
